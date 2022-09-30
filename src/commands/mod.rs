430efc3d0cad5e3611d26da30937941d73a04e6f
xsb <389483673@qq.com>
2022-09-30 19:23:43 +0800

    docs: update readme

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -2,7 +2,7 @@ use strum::{EnumDiscriminants, EnumIter, EnumMessage};
 
 pub mod deploy_upgrade_command;
 pub mod clean_state_command;
-pub mod check_rewards;
+pub mod check_unprofitable_validator;
 
 #[derive(Debug, Clone, EnumDiscriminants, interactive_clap_derive::InteractiveClap)]
 #[strum_discriminants(derive(EnumMessage, EnumIter))]
@@ -14,8 +14,8 @@ pub enum TopLevelCommand {
     DeployOrUpgrade(self::deploy_upgrade_command::DeployOrUpgrade),
     #[strum_discriminants(strum(message = "Clean state"))]
     CleanState(self::clean_state_command::CleanStateCommand),
-    #[strum_discriminants(strum(message = "Check reward status"))]
-    CheckReward(self::check_rewards::CheckReward),
+    #[strum_discriminants(strum(message = "Check unprofitable validator"))]
+    CheckUnprofitableValidator(self::check_unprofitable_validator::CheckUnprofitableValidator),
 
 }
 
@@ -25,7 +25,7 @@ impl TopLevelCommand {
             TopLevelCommand::DeployOrUpgrade(anchor_upgrade_command) => anchor_upgrade_command.process().await,
 
             TopLevelCommand::CleanState(clean_state_command) => clean_state_command.process().await,
-            TopLevelCommand::CheckReward(check_rewards) => check_rewards.process().await
+            TopLevelCommand::CheckUnprofitableValidator(check_unprofitable_validator) => check_unprofitable_validator.process().await
         }
     }
 }
