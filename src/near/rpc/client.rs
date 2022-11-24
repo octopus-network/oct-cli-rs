7b47128c3b25c24c9f4b04e964ffb69130def043
Rivers Yang <rivers.yang@icloud.com>
2022-11-24 10:32:36 +0800

    Upgrade version of near crates.

--- src/near/rpc/client.rs
+++ src/near/rpc/client.rs
@@ -189,6 +189,7 @@ impl Client {
                 request: QueryRequest::ViewState {
                     account_id: contract_id,
                     prefix: StoreKey::from(prefix.map(Vec::from).unwrap_or_default()),
+                    include_proof: false,
                 },
             })
             .await?;
